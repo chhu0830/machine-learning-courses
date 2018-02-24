52a5fdccc55de827d55bb4a6cd540cc450e1d48b
oHSUANo <chhu0830@gmail.com>
2018-02-24 23:40:29 +0800

    Finish svm practice

--- ud120/assignments/svm/svm_author_id.py
+++ ud120/assignments/svm/svm_author_id.py
@@ -24,6 +24,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+
+clf = SVC(kernel="linear")          # 88.5% with 1% data, 98.4% with 100% data
+clf = SVC(kernel="rbf")             # 61.6% with 1% data
+clf = SVC(kernel="rbf", C=10000)    # 89.2% with 1% data, 99.1% with 100% data
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)*100, "%"
 
 #########################################################
 
